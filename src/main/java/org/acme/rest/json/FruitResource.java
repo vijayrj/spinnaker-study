db9a357c39e1ebdb89745df717f2aaf637a675f8
Vijaya Jallu <vjallu@salesforce.com>
2021-04-29 12:10:56 -0700

    code change to trigger spinnaker deployment

--- src/main/java/org/acme/rest/json/FruitResource.java
+++ src/main/java/org/acme/rest/json/FruitResource.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Set;
 
+import org.jboss.logging.Logger;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -17,6 +19,7 @@ import javax.ws.rs.core.MediaType;
 @Consumes(MediaType.APPLICATION_JSON)
 public class FruitResource {
 
+    private static final Logger LOG = Logger.getLogger(FruitResource.class);
     private Set<Fruit> fruits = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
 
     public FruitResource() {
@@ -26,6 +29,7 @@ public class FruitResource {
 
     @GET
     public Set<Fruit> list() {
+        LOG.infof("Returning fruits " + fruits);        
         return fruits;
     }
 
